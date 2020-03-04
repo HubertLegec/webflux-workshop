16038cfee7f22446f0a58be841f71e9fcf733ba7
Hubert Legęć <hubert.legec@gmail.com>
2020-03-05 00:07:16 +0100

    Initial commit

---
+++ src/test/java/com/legec/webfluxworkshop/WebfluxWorkshopApplicationTests.java
@@ -0,0 +1,21 @@
+package com.legec.webfluxworkshop;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+@SpringBootTest
+class WebfluxWorkshopApplicationTests {
+
+    @Test
+    void firstMonoTest() {
+        Assertions.fail();
+    }
+
+    @Test
+    void firstFluxTest() {
+        Assertions.fail();
+    }
+}
